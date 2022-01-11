162c562831fa89e06ef706acaddf01699c485105
Ben Lu <ayroblu@gmail.com>
2022-01-11 20:15:40 +0000

    Use hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { App } from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename=".">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
