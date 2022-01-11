fa96fc7de37c9f0573de17dc8485db62e8379235
Ben Lu <ayroblu@gmail.com>
2022-01-11 20:22:43 +0000

    Remove basename

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { App } from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename=".">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
