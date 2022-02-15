c4a10f31274720baaef36166a552a6ae8ee7501b
Ben Lu <ayroblu@gmail.com>
2022-02-15 22:33:04 +0000

    Setup basic test

---
+++ tests/first-time-setup.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+const name = "first-time-setup";
+
+describe(name, () => {
+  test("should successfully create a file when prompted", async ({ page }) => {
+    await page.goto("http://localhost:8080");
+    const button = page.locator("text=Click me");
+    await button.waitFor();
+    expect(await page.screenshot()).toMatchSnapshot([name, "click-button.png"]);
+    await button.click();
+    await page.locator("text=playground for typing").waitFor();
+  });
+});
