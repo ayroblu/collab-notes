eeb3afb0a4d1977c928559b9b69a65ee9596ca45
Ben Lu <ayroblu@gmail.com>
2022-02-16 20:20:51 +0000

    update test

--- tests/first-time-setup.spec.ts
+++ tests/first-time-setup.spec.ts
@@ -5,19 +5,24 @@ const name = "first-time-setup";
 test.describe(name, () => {
   test("should successfully create a file when prompted", async ({ page }) => {
     await page.goto("http://localhost:8080");
+    await page.locator('[placeholder="filename.ts"]').waitFor();
+    expect(await page.screenshot()).toMatchSnapshot([
+      name,
+      "1-initial-load.png",
+    ]);
     await page.locator('[placeholder="filename.ts"]').type("tweet.ts");
     await page.keyboard.press("Enter");
     await page.locator('[data-mode-id="typescript"]').waitFor();
     expect(await page.screenshot()).toMatchSnapshot([
       name,
-      "1-typescript-loaded.png",
+      "2-typescript-loaded.png",
     ]);
     await page.fill(".monaco-editor textarea", content);
     // await page.type("input", content);
     // await page.fill("input", content);
     expect(await page.screenshot()).toMatchSnapshot([
       name,
-      "2-content-added.png",
+      "3-content-added.png",
     ]);
   });
 });
