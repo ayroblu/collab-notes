fe533ef34bf5de8da64f05c0b5a8513f9e6cd3f4
Ben Lu <ayroblu@gmail.com>
2022-02-19 18:01:05 +0000

    Remove unused hooks

--- src/hooks/useEffectOnce.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const useEffectOnce = (func: () => void) => {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  React.useEffect(func, []);
-};
--- src/hooks/useLayoutEffectOnce.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const useLayoutEffectOnce = (func: () => void) => {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  React.useLayoutEffect(func, []);
-};
